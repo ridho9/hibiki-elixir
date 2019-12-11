b567103152ac879cb1c73c7c49c6f7f0532831be
Ridho Pratama <p.ridho@yahoo.co.id>
2019-12-11 23:05:21 +0700

    linesdk: remover starter code

--- apps/line_sdk/lib/line_sdk.ex
+++ apps/line_sdk/lib/line_sdk.ex
@@ -2,17 +2,4 @@ defmodule LineSdk do
   @moduledoc """
   Documentation for LineSdk.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LineSdk.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- apps/line_sdk/test/line_sdk_test.exs
+++ apps/line_sdk/test/line_sdk_test.exs
@@ -1,8 +1,4 @@
 defmodule LineSdkTest do
   use ExUnit.Case
   doctest LineSdk
-
-  test "greets the world" do
-    assert LineSdk.hello() == :world
-  end
 end
