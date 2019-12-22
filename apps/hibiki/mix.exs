6ccf8d9875e65f19f7178c859c6b31110da7ca18
Ridho Pratama <p.ridho@yahoo.co.id>
2019-12-23 01:01:50 +0700

    removed raxx server

--- apps/hibiki/mix.exs
+++ apps/hibiki/mix.exs
@@ -29,7 +29,6 @@ defmodule Hibiki.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
-      {:raxx, "~> 1.1"}
     ]
   end
 end
--- apps/server/test/server_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule ServerTest do
-  use ExUnit.Case
-  doctest Server
-end
