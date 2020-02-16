22fc7473c92363122dab5bfff1eff3ebafa87c54
Ridho Pratama <p.ridho@yahoo.co.id>
2020-02-16 22:47:20 +0700

    fixed unused parameter

--- apps/hibiki_web/lib/hibiki_web/page/not_found.ex
+++ apps/hibiki_web/lib/hibiki_web/page/not_found.ex
@@ -2,7 +2,7 @@ defmodule HibikiWeb.Page.NotFound do
   use Raxx.SimpleServer
 
   @impl Raxx.SimpleServer
-  def handle_request(_req, state) do
+  def handle_request(_req, _state) do
     response(:not_found)
     |> set_header("content-type", "text/plain")
     |> set_body("content not found")
