428803a0e3f42c032f1f51fa9ea53795ebbdf92a
Ridho Pratama <p.ridho@yahoo.co.id>
2020-06-03 17:55:21 +0700

    added teitoku

---
+++ apps/teitoku/lib/teitoku.ex
@@ -0,0 +1,18 @@
+defmodule Teitoku do
+  @moduledoc """
+  Documentation for `Teitoku`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Teitoku.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/teitoku/test/teitoku_test.exs
@@ -0,0 +1,8 @@
+defmodule TeitokuTest do
+  use ExUnit.Case
+  doctest Teitoku
+
+  test "greets the world" do
+    assert Teitoku.hello() == :world
+  end
+end
