43da78e4585f0d04d000a849956297da8fd06bcb
Ridho Pratama <p.ridho@yahoo.co.id>
2020-06-15 15:41:20 +0700

    hibiki upload added caching

--- apps/hibiki/lib/hibiki/command_libs/upload/upload.ex
+++ apps/hibiki/lib/hibiki/command_libs/upload/upload.ex
@@ -1,5 +1,6 @@
 defmodule Hibiki.Upload do
   alias Hibiki.Upload.Provider.Catbox
+  alias Hibiki.Cache
 
   @spec upload_binary(module, binary) :: {:ok, String.t()} | {:error, any}
   def upload_binary(provider, binary) do
@@ -10,6 +11,22 @@ defmodule Hibiki.Upload do
   def upload_base64_to_catbox(string) do
     with {:ok, binary} <- Base.decode64(string) do
       Catbox.upload_binary(binary)
+    end
+  end
+
+  def upload_from_image_id(provider, image_id) do
+    cache_key = Cache.Key.uploaded_image_url(image_id, provider.id)
+
+    case Cache.get(cache_key) do
+      nil ->
+        with {:ok, image_binary} <- LineSdk.Client.get_content(image_id),
+             {:ok, image_url} <- upload_binary(provider, image_binary) do
+          Cache.set(cache_key, image_url)
+          {:ok, image_url}
+        end
+
+      image_url ->
+        {:ok, image_url}
     end
   end
 end
