88833faeadd0d5acf4dee4f8d5e1cda01a0f5218
Ridho Pratama <p.ridho@yahoo.co.id>
2020-06-17 19:34:32 +0700

    record last sent image by entity

--- apps/hibiki/lib/hibiki/event/image.ex
+++ apps/hibiki/lib/hibiki/event/image.ex
@@ -4,11 +4,15 @@ defmodule Hibiki.Event.Image do
 end
 
 defimpl Teitoku.HandleableEvent, for: Hibiki.Event.Image do
-  def handle(%Hibiki.Event.Image{id: _id}, _ctx) do
+  def handle(%Hibiki.Event.Image{id: id}, %{source: source}) do
     # {:reply,
     #  %LineSdk.Model.TextMessage{
     #    text: "image id #{id}"
     #  }}
+
+    source
+    |> Hibiki.Entity.from_source()
+    |> Hibiki.Entity.Data.set(Hibiki.Entity.Data.Key.last_image_id(), id)
 
     {:ignore, nil}
   end
