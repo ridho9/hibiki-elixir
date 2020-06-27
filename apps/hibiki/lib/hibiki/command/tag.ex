4e3dadf51fc7a693756ffc129b984fa9361b462f
Ridho Pratama <p.ridho@yahoo.co.id>
2020-06-27 22:01:46 +0700

    hibiki tag get add raw

--- apps/hibiki/lib/hibiki/command/tag.ex
+++ apps/hibiki/lib/hibiki/command/tag.ex
@@ -11,17 +11,29 @@ defmodule Hibiki.Command.Tag do
     do:
       %Opt{}
       |> Opt.add_named("name", desc: "Tag name")
+      |> Opt.add_flag("r", desc: "Raw value")
 
-  def handle(%{"name" => name}, %{source: source, user: user}) do
+  def handle(%{"name" => name, "r" => raw}, %{source: source, user: user}) do
     case Tag.get_from_tiered_scope(name, source, user) do
       nil ->
         {:reply_error, "Tag '#{name}' not found in this #{source.type}"}
 
-      %Tag{name: name, value: value, type: type} ->
-        {:reply,
-         %LineSdk.Model.TextMessage{
-           text: "#{name} #{value} #{type}"
-         }}
+      %Tag{value: value, type: type} ->
+        msg =
+          cond do
+            raw || type == "text" ->
+              %LineSdk.Model.TextMessage{
+                text: value
+              }
+
+            type == "image" ->
+              %LineSdk.Model.ImageMessage{
+                preview_url: value,
+                original_url: value
+              }
+          end
+
+        {:reply, msg}
     end
   end
 end
