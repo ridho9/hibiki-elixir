a4ebc14d9fa010c3684d3d487ee563f38a53104c
Ridho Pratama <p.ridho@yahoo.co.id>
2020-07-25 14:46:02 +0700

    Hibiki fix error when entity have does not exist

--- apps/hibiki/lib/hibiki/converter_impl.ex
+++ apps/hibiki/lib/hibiki/converter_impl.ex
@@ -1,11 +1,26 @@
 defimpl Hibiki.Convertable, for: LineSdk.Model.MessageEvent do
   alias LineSdk.Model.Source
+  alias Hibiki.Entity
 
   def convert(%LineSdk.Model.MessageEvent{message: message, source: source}, ctx) do
+    user_entity = source |> Source.user_id() |> Entity.create_or_get("user")
+
+    source_entity =
+      case source do
+        %LineSdk.Model.SourceGroup{group_id: source_id} ->
+          Entity.create_or_get(source_id, "group")
+
+        %LineSdk.Model.SourceRoom{room_id: source_id} ->
+          Entity.create_or_get(source_id, "room")
+
+        %LineSdk.Model.SourceUser{} ->
+          user_entity
+      end
+
     ctx =
       ctx
-      |> Map.put(:source, source |> Source.id() |> Hibiki.Entity.get())
-      |> Map.put(:user, source |> Source.user_id() |> Hibiki.Entity.get())
+      |> Map.put(:source, source_entity)
+      |> Map.put(:user, user_entity)
 
     Hibiki.Convertable.convert(message, ctx)
   end
