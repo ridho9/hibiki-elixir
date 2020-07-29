b61d5d5ba9bd78a96600b6a27f46ca5020bf01f4
Ridho Pratama <p.ridho@yahoo.co.id>
2020-07-29 17:52:10 +0700

    added releases files

---
+++ config/releases.exs
@@ -0,0 +1,15 @@
+import Config
+
+config :hibiki,
+  admin_id: System.fetch_env!("ADMIN_ID") |> String.split(","),
+  channel_access_token: System.fetch_env!("CHANNEL_ACCESS_TOKEN"),
+  channel_secret: System.fetch_env!("CHANNEL_SECRET")
+
+config :hibiki_web,
+  port: 8080
+
+config :hibiki, Hibiki.Repo,
+  database: System.fetch_env!("DB_NAME"),
+  username: System.fetch_env!("DB_USERNAME"),
+  password: System.fetch_env!("DB_PASSWORD"),
+  hostname: System.fetch_env!("DB_HOSTNAME")
--- mix.exs
+++ mix.exs
@@ -4,9 +4,17 @@ defmodule HibikiElixir.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        hibiki_elixir: [
+          version: "1.0.0",
+          applications: [
+            hibiki: :permanent,
+            hibiki_web: :permanent
+          ]
+        ]
+      ]
     ]
   end
 
