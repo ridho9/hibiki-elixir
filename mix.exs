5af216ec2f8b534c9d340e1a54f7664cac9322ba
Ridho Pratama <p.ridho@yahoo.co.id>
2020-08-03 21:41:16 +0700

    bump 1.10

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule HibikiElixir.MixProject do
       deps: deps(),
       releases: [
         hibiki_elixir: [
-          version: "1.9.0",
+          version: "1.10.0",
           applications: [
             hibiki: :permanent,
             hibiki_web: :permanent
