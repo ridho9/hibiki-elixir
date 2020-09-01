25f67f38c641ba9015a603704fdb8881be61805a
Ridho Pratama <p.ridho@yahoo.co.id>
2020-09-01 12:31:48 +0700

    fix: add httpoison options to post call in !tl

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,4 @@
 ### Changed
 - Changed #tag# to ignore if the last char inside is space (like #tag #tag)
 - Removed unnecessary logging in tenshi provider
+- Add httpoison options to post call in !tl
--- apps/hibiki/lib/hibiki/command/tl.ex
+++ apps/hibiki/lib/hibiki/command/tl.ex
@@ -14,8 +14,14 @@ defmodule Hibiki.Command.Tl do
   def handle(%{"query" => query}, _ctx) do
     url = Hibiki.Config.deepl_proxy() <> "/translate/tl"
 
+    http_config = [
+      hackney: [pool: :tl],
+      recv_timeout: 30_000,
+      timeout: 30_000
+    ]
+
     with {:ok, body} <- Jason.encode(%{"src" => query}),
-         {:ok, %HTTPoison.Response{body: body}} <- HTTPoison.post(url, body),
+         {:ok, %HTTPoison.Response{body: body}} <- HTTPoison.post(url, body, [], http_config),
          {:ok, %{"result" => result, "src_lang" => src_lang}} <- Jason.decode(body) do
       {:reply,
        %LineSdk.Model.TextMessage{
