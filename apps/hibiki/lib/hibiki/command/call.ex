8b61f389640e09a26d35b57492211234638e4375
Ridho Pratama <p.ridho@yahoo.co.id>
2020-09-29 21:17:16 +0700

    fix: remove call timer

--- apps/hibiki/lib/hibiki/command/call.ex
+++ apps/hibiki/lib/hibiki/command/call.ex
@@ -6,8 +6,6 @@ defmodule Hibiki.Command.Call do
   def description, do: "A simple call"
 
   def handle(_args, _ctx) do
-    :timer.sleep(6000)
-
     {:reply,
      %LineSdk.Model.TextMessage{
        text: "Roger, Hibiki, heading out!\n\nI'll never forget Tenshi..."
