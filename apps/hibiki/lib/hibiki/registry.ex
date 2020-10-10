d87aed183c20767d91158f368081ee91af877c33
Ridho Pratama <p.ridho@yahoo.co.id>
2020-10-10 08:48:32 +0700

    feat: hibiki.registry autodiscover

--- apps/hibiki/lib/hibiki/registry.ex
+++ apps/hibiki/lib/hibiki/registry.ex
@@ -1,19 +1,20 @@
 defmodule Hibiki.Registry do
-  alias Hibiki.Command, as: Cmd
+  def subcommands, do: default_commands()
 
-  def subcommands,
-    do: [
-      Cmd.Call,
-      Cmd.Calc,
-      Cmd.Chs,
-      Cmd.Help,
-      Cmd.Case,
-      Cmd.Info,
-      Cmd.Upload,
-      Cmd.Tag,
-      Cmd.Code,
-      Cmd.Sauce,
-      Cmd.Translate,
-      Cmd.Ryn
-    ]
+  def default_commands do
+    app = Application.get_application(__MODULE__)
+    {:ok, modules} = :application.get_key(app, :modules)
+
+    command_modules =
+      modules
+      |> Enum.filter(&Teitoku.Command.command?/1)
+
+    children_modules =
+      Enum.reduce(command_modules, [], fn x, acc ->
+        acc ++ x.subcommands()
+      end)
+
+    command_modules
+    |> Enum.filter(fn module -> module not in children_modules end)
+  end
 end
