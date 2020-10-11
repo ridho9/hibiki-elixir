b32c24086f86807cc7dd8574edd814e4f08fff46
Ridho Pratama <p.ridho@yahoo.co.id>
2020-10-11 11:52:30 +0700

    feat: refactor hibiki.registry to teitoku.registry

--- apps/hibiki/lib/hibiki/registry.ex
+++ apps/hibiki/lib/hibiki/registry.ex
@@ -1,20 +1,3 @@
 defmodule Hibiki.Registry do
-  def subcommands, do: default_commands()
-
-  def default_commands do
-    app = Application.get_application(__MODULE__)
-    {:ok, modules} = :application.get_key(app, :modules)
-
-    command_modules =
-      modules
-      |> Enum.filter(&Teitoku.Command.command?/1)
-
-    children_modules =
-      Enum.reduce(command_modules, [], fn x, acc ->
-        acc ++ x.subcommands()
-      end)
-
-    command_modules
-    |> Enum.filter(fn module -> module not in children_modules end)
-  end
+  use Teitoku.Command.Registry
 end
