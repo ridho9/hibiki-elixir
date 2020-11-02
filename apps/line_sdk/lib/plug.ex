437aca8af3bbb7627a96ab07d1892e1d323969ae
Ridho Pratama <p.ridho@yahoo.co.id>
2020-11-02 11:18:16 +0700

    debug

--- apps/line_sdk/lib/plug.ex
+++ apps/line_sdk/lib/plug.ex
@@ -20,7 +20,7 @@ defmodule LineSdk.Plug do
         opts
       ) do
     with {:ok, signature} <- get_signature(conn),
-         IO.inspect("sign #{signature} body #{raw_body} opts #{opts}"),
+         IO.inspect("sign #{signature} body #{raw_body} opts #{inspect(opts)}"),
          {:ok, _} <-
            LineSdk.Auth.validate_message(
              raw_body,
