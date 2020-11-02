1f9e30fda40b52b4fa91282cd02d7a2a0adac0aa
Ridho Pratama <p.ridho@yahoo.co.id>
2020-11-02 15:33:06 +0700

    Change the passed opts to function

--- apps/hibiki_web/lib/hibiki_web/router.ex
+++ apps/hibiki_web/lib/hibiki_web/router.ex
@@ -12,7 +12,7 @@ defmodule HibikiWeb.Router do
   post("/hibiki",
     to: LineSdk.Plug,
     init_opts: [
-      channel_secret: Hibiki.Config.channel_secret(),
+      channel_secret: &Hibiki.Config.channel_secret/0,
       handler: Hibiki.WebhookHandler
     ]
   )
