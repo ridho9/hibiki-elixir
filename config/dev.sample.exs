1d864422bd9373267faee5d4bb7e0a25ab7aecf7
Ridho Pratama <p.ridho@yahoo.co.id>
2021-10-19 17:34:15 +0700

    Added sample dev config file

---
+++ config/dev.sample.exs
@@ -0,0 +1,19 @@
+import Config
+
+config :hibiki,
+  admin_id: ["admin_id"],
+  channel_access_token: "channel access token",
+  channel_secret: "channel secret",
+  deepl_proxy: "deepl proxy, ignore"
+
+config :hibiki,
+  tag: "DEV"
+
+config :hibiki_web,
+  port: 8080
+
+config :hibiki, Hibiki.Repo,
+  database: "hibiki",
+  username: "postgres",
+  password: "docker",
+  hostname: "localhost"
